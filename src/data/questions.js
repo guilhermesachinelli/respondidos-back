85aa037af8663f3ee6a1762d8568f7f38a71cdd0
GuiLimaSeila <guilhermehenriquelima@gmail.com>
2023-11-30 10:41:09 -0300

    feat: aboutus

--- src/data/questions.js
+++ src/data/questions.js
@@ -28,5 +28,265 @@ export const questions = [
         response3 : "Monte Elbert",
         response4 : "Monte Rainier",
         correct : "Denali (Monte McKinley)"
+    },
+    {
+        question : "Qual seleção venceu a Copa do Mundo de Futebol de 2018?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "França",
+        response2 : "Argentina",
+        response3 : "Itália",
+        response4 : "Brasil",
+        correct : "França"
+    },
+    {
+        question : "Quem é o maior artilheiro da história da Liga dos Campeões da UEFA?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Lionel Messi",
+        response2 : "Neymar Jr",
+        response3 : "Cristiano Ronaldo",
+        response4 : "Erlin Haaland",
+        correct : "Cristiano Ronaldo"
+    },
+    {
+        question : "Qual clube brasileiro conquistou mais títulos do Mundial de Clubes",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Palmeiras",
+        response2 : "Corinthians",
+        response3 : "São Paulo",
+        response4 : "Grêmio",
+        correct : "São Paulo"
+    },
+    {
+        question : "Quem ganhou o prêmio de Melhor Jogador da FIFA em 1984?",
+        category: "Esportes",
+        difficulty: "Difícil",	
+        response1 : "Ruud Gullit",
+        response2 : "Michel Platini",
+        response3 : "Rivaldo",
+        response4 : "Roberto Baggio",
+        correct : "Michel Platini"
+    },
+    {
+        question : "Em que ano o Brasil sediou a Copa do Mundo pela primeira vez?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "2002",
+        response2 : "1994",
+        response3 : "2014",
+        response4 : "1950",
+        correct : "1950"
+    },
+    {
+        question : "Quem é o técnico da seleção alemã de futebol masculino em 2023?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Joachim Löw",
+        response2 : "Julian Nagelsmann",
+        response3 : "Hansi Flick",
+        response4 : "Pep Guardiola",
+        correct : "Julian Nagelsmann"
+    },
+    {
+        question : "Quem detém o recorde de mais gols marcados em uma única edição da Copa do Mundo?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Pelé",
+        response2 : "Miroslav Klose",
+        response3 : "Ronaldo Nazário",
+        response4 : "Lionel Messi",
+        correct : "Miroslav Klose"
+    },
+    {
+        question : "Em que ano foi fundada a FIFA, a Federação Internacional de Futebol?",
+        category: "Esportes",
+        difficulty: "Difícil",	
+        response1 : "1902",
+        response2 : "1914",
+        response3 : "1930",
+        response4 : "1904",
+        correct : "1904"
+    },
+    {
+        question : "Qual é o tamanho oficial da quadra de vôlei em metros para jogos indoor?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "15m x 7m",
+        response2 : "18m x 9m",
+        response3 : "20m x 10m",
+        response4 : "25m x 12m",
+        correct : "18m x 9m"
+    },
+    {
+        question : "Quantos jogadores compõem uma equipe de vôlei em quadra durante uma partida?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "4",
+        response2 : "6",
+        response3 : "8",
+        response4 : "5",
+        correct : "6"
+    },
+    {
+        question : "Em que país teve origem o vôlei?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Brasil",
+        response2 : "Estados Unidos",
+        response3 : "Rússia",
+        response4 : "Itália",
+        correct : "Estados Unidos"
+    },
+    {
+        question : "Qual é a altura oficial da rede de vôlei para homens e mulheres?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "2,40m (homens) e 2,20m (mulheres)",
+        response2 : "2,50m (homens) e 2,40m (mulheres)",
+        response3 : "2,35m (homens) e 2,15m (mulheres)",
+        response4 : "2,45m (homens) e 2,25m (mulheres)",
+        correct : "2,40m (homens) e 2,20m (mulheres)"
+    },
+    {
+        question : "Quem venceu a última edição dos Jogos Olímpicos no vôlei feminino em 2020 (realizado em 2021)?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Brasil",
+        response2 : "Estados Unidos",
+        response3 : "China",
+        response4 : "Rússia",
+        correct : "Estados Unidos"
+    },
+    {
+        question : "Qual seleção brasileira de vôlei conquistou a medalha de ouro nos Jogos Olímpicos de 2016?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Masculina",
+        response2 : "Feminina",
+        response3 : "Ambas",
+        response4 : "Nenhuma",
+        correct : "Ambas"
+    },
+    {
+        question : "Em que cidade foram realizados os primeiros Jogos Olímpicos da era moderna em 1896?",
+        category: "Esportes",
+        difficulty: "Difícil",	
+        response1 : "Roma",
+        response2 : "Atenas",
+        response3 : "Paris",
+        response4 : "Londres",
+        correct : "Atenas"
+    },
+    {
+        question : "Quem é o deus grego que inspirou o nome dos Jogos Olímpicos na antiguidade?",
+        category: "Esportes",
+        difficulty: "Fácil",
+        response1 : "Zeus",
+        response2 : "Apolo",
+        response3 : "Hermes",
+        response4 : "Ares",
+        correct : "Zeus"
+    },
+    {
+        question : "Qual é o símbolo das Olimpíadas que representa a união dos cinco continentes?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Chama Olímpica",
+        response2 : "Tocha Olímpica",
+        response3 : "Bandeira Olímpica",
+        response4 : "Anéis Olímpicos",
+        correct : "Anéis Olímpicos"
+    },
+    {
+        question : "O que é a Tocha Olímpica e qual é o significado por trás desse símbolo?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Representa a paz e a união entre os atletas.",
+        response2 : "Simboliza a chama olímpica durante a cerimônia de abertura.",
+        response3 : "Representa a conexão entre os continentes.",
+        response4 : "Simboliza a passagem da antiga tradição para a modernidade.",
+        correct : "Representa a paz e a união entre os atletas."
+    },
+    {
+        question : "Quem foi o atleta mais medalhado na história dos Jogos Olímpicos de Verão?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Usain Bolt",
+        response2 : "Michael Phelps",
+        response3 : "Simone Biles",
+        response4 : "Carl Lewis",
+        correct : "Michael Phelps"
+    },
+    {
+        question : "Qual é o esporte que utiliza uma pista inclinada com curvas fechadas e é praticado em trenós?",
+        category: "Esportes",
+        difficulty: "Difícil",	
+        response1 : "Luge",
+        response2 : "Skeleton",
+        response3 : "Esqui Cross-Country",
+        response4 : "Monte Rainier",
+        correct : "Luge"
+    },
+    {
+        question : "Em qual esporte você pode marcar um touchdown?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Basquete",
+        response2 : "Futebol Americano",
+        response3 : "Beisebol",
+        response4 : "Golfe",
+        correct : "Futebol Americano"
+    },
+    {
+        question : "Quem é considerado o Rei do Tênis e detém diversos recordes no esporte?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Rafael Nadal",
+        response2 : "Novak Djokovic",
+        response3 : "Roger Federer",
+        response4 : "Andy Murray",
+        correct : "Roger Federer"
+    },
+    {
+        question : "Em que país nasceu o esporte conhecido como Muay Thai?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Japão",
+        response2 : "Tailândia",
+        response3 : "Brasil",
+        response4 : "China",
+        correct : "Tailândia"
+    },
+    {
+        question : "Quem é considerado o Rei do Xadrez e detém o título de Campeão Mundial?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Garry Kasparov",
+        response2 : "Bobby Fischer",
+        response3 : "Viswanathan Anand",
+        response4 : "Magnus Carlsen",
+        correct : "Magnus Carlsen"
+    },
+    {
+        question : "Qual é o esporte que envolve uma pista de gelo e uma equipe deslizando pedras em direção a um alvo circular?",
+        category: "Esportes",
+        difficulty: "Médio",	
+        response1 : "Curling",
+        response2 : "Patinação Artística",
+        response3 : "Hóquei no Gelo",
+        response4 : "Bobsleigh",
+        correct : "Curling"
+    },
+    {
+        question : "Qual é o esporte eletrônico (E-sport) que simula partidas de futebol virtual?",
+        category: "Esportes",
+        difficulty: "Fácil",	
+        response1 : "Counter-Strike: Global Offensive",
+        response2 : "League of Legends",
+        response3 : "FIFA",
+        response4 : "Dota 2",
+        correct : "FIFA"
     }
 ];
